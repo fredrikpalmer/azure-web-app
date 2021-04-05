431eb1d2b158254bcbad6161cedbf5cedb56ffbe
fredrikpalmer <fredrik.palmer@telia.com>
2021-04-05 14:51:46 +0200

    cleanup

--- main.tf
+++ main.tf
@@ -5,21 +5,26 @@ data "azuread_group" "developers" {
 }
 
 locals {
+  name = "${var.org}-${var.team}-${var.domain}-${var.context}"
   tags = {
+    "organization" = var.org
+    "team" = var.team
+    "domain" = var.domain
+    "context" = var.context
     "environment" = var.env
   }
 }
 
-resource "azurerm_resource_group" "app-rg" {
- name = "${var.env}-${var.app}-rg" 
+resource "azurerm_resource_group" "app_rg" {
+ name = "${local.name}-rg" 
  location = var.location
 
  tags = local.tags
 }
 
-resource "azurerm_app_service_plan" "app-service-plan" {
-  name = "${var.env}-${var.app}-plan"
-  resource_group_name = azurerm_resource_group.app-rg.name
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name = "${local.name}-plan"
+  resource_group_name = azurerm_resource_group.app_rg.name
   location = var.location
   kind = "Linux"
   reserved = true
@@ -32,22 +37,22 @@ resource "azurerm_app_service_plan" "app-service-plan" {
   tags = local.tags
 }
 
-resource "azurerm_app_service" "app-service" {
-  name = "${var.env}-${var.app}-service"
-  app_service_plan_id = azurerm_app_service_plan.app-service-plan.id
-  resource_group_name = azurerm_resource_group.app-rg.name
+resource "azurerm_app_service" "app_service" {
+  name = "${local.name}-service"
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  resource_group_name = azurerm_resource_group.app_rg.name
   location = var.location
 
   site_config {
-    linux_fx_version = "DOCKER|${azurerm_container_registry.app-container-registry.login_server}/${var.app}:latest"
+    linux_fx_version = "DOCKER|${azurerm_container_registry.app_container_registry.login_server}/${var.domain}-${var.context}:latest"
     always_on        = "true"
   }
 
   app_settings = {    
-    Secret = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.app-key-vault.vault_uri}secrets/${azurerm_key_vault_secret.app-key-vault-secret.name}/${azurerm_key_vault_secret.app-key-vault-secret.version})"
-    DOCKER_REGISTRY_SERVER_URL = "https://${azurerm_container_registry.app-container-registry.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.app-container-registry.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.app-container-registry.admin_password
+    Secret = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.app_key_vault.vault_uri}secrets/${azurerm_key_vault_secret.app_key_vault_secret.name}/${azurerm_key_vault_secret.app_key_vault_secret.version})"
+    DOCKER_REGISTRY_SERVER_URL = "https://${azurerm_container_registry.app_container_registry.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.app_container_registry.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.app_container_registry.admin_password
   }
 
   identity {
@@ -63,9 +68,9 @@ resource "azurerm_app_service" "app-service" {
  }
 }
 
-resource "azurerm_key_vault" "app-key-vault" {
-  name = "${var.env}-${var.app}-kv"
-  resource_group_name = azurerm_resource_group.app-rg.name
+resource "azurerm_key_vault" "app_key_vault" {
+  name = "${local.name}-kv"
+  resource_group_name = azurerm_resource_group.app_rg.name
   location = var.location
   tenant_id = var.tenant_id
   sku_name = "standard"
@@ -73,32 +78,32 @@ resource "azurerm_key_vault" "app-key-vault" {
   tags = local.tags
 }
 
-resource "azurerm_key_vault_access_policy" "app-key-vault-access-policy-terraform" {
-  key_vault_id = azurerm_key_vault.app-key-vault.id
+resource "azurerm_key_vault_access_policy" "app_key_vault_access_policy" {
+  key_vault_id = azurerm_key_vault.app_key_vault.id
+  object_id = azurerm_app_service.app_service.identity[0].principal_id
+  tenant_id = azurerm_app_service.app_service.identity[0].tenant_id
+  secret_permissions = [ "get", "list" ]
+}
+
+resource "azurerm_key_vault_access_policy" "app_key_vault_access_policy_terraform" {
+  key_vault_id = azurerm_key_vault.app_key_vault.id
   object_id = data.azurerm_client_config.current.object_id
   tenant_id = data.azurerm_client_config.current.tenant_id
   secret_permissions = [ "delete", "get", "set", "purge" ]
 }
 
-resource "azurerm_key_vault_access_policy" "app-key-vault-access-policy-application" {
-  key_vault_id = azurerm_key_vault.app-key-vault.id
-  object_id = azurerm_app_service.app-service.identity[0].principal_id
-  tenant_id = azurerm_app_service.app-service.identity[0].tenant_id
-  secret_permissions = [ "get", "list" ]
-}
-
-resource "azurerm_key_vault_access_policy" "app-key-vault-access-policy-developers" {
-  key_vault_id = azurerm_key_vault.app-key-vault.id
+resource "azurerm_key_vault_access_policy" "app_key_vault-access_policy_developers" {
+  key_vault_id = azurerm_key_vault.app_key_vault.id
   object_id = data.azuread_group.developers.object_id
-  tenant_id = azurerm_app_service.app-service.identity[0].tenant_id
+  tenant_id = azurerm_app_service.app_service.identity[0].tenant_id
   secret_permissions = [ "delete", "get", "set", "purge", "list" ]
 }
 
-resource "azurerm_key_vault_secret" "app-key-vault-secret" {
-  name = "${var.env}-${var.app}-kv-secret"
-  key_vault_id = azurerm_key_vault.app-key-vault.id
+resource "azurerm_key_vault_secret" "app_key_vault_secret" {
+  name = "${local.name}-kv-secret"
+  key_vault_id = azurerm_key_vault.app_key_vault.id
   value = "secret"
-  depends_on = [azurerm_key_vault_access_policy.app-key-vault-access-policy-terraform]
+  depends_on = [azurerm_key_vault_access_policy.app_key_vault_access_policy_terraform]
 
   tags = local.tags
 
@@ -109,9 +114,9 @@ resource "azurerm_key_vault_secret" "app-key-vault-secret" {
  }
 }
 
-resource "azurerm_container_registry" "app-container-registry" {
-  name = "${var.app}acr${var.env}"
-  resource_group_name = azurerm_resource_group.app-rg.name
+resource "azurerm_container_registry" "app_container_registry" {
+  name = "${var.org}${var.team}"
+  resource_group_name = azurerm_resource_group.app_rg.name
   location = var.location
   sku = "Standard"
   admin_enabled = true
@@ -119,8 +124,8 @@ resource "azurerm_container_registry" "app-container-registry" {
   tags = local.tags
 }
 
-resource "azurerm_role_assignment" "app-role-assigment-acrpull" {
-  principal_id = azurerm_app_service.app-service.identity[0].principal_id
-  scope = azurerm_container_registry.app-container-registry.id
+resource "azurerm_role_assignment" "app_role_assigment_acrpull" {
+  principal_id = azurerm_app_service.app_service.identity[0].principal_id
+  scope = azurerm_container_registry.app_container_registry.id
   role_definition_name = "AcrPull"
 }
--- variables.tf
+++ variables.tf
@@ -24,14 +24,32 @@ variable "location" {
   default = "northeurope"
 }
 
+variable "org" {
+  type = string
+  description = "Name of the app"
+  default = "fp"
+}
+
+variable "team" {
+  type = string
+  description = "Name of the team"
+  default = "team"
+}
+
+variable "domain" {
+  type = string
+  description = "Name of the domain"
+  default = "domain"
+}
+
+variable "context" {
+  type = string
+  description = "Name of the context"
+  default = "api"
+}
+
 variable "env" {
   type = string
   description = "Environment of the resources"
   default = "dev"
-}
-
-variable "app" {
-  type = string
-  description = "Name of the app"
-  default = "app"
 }
